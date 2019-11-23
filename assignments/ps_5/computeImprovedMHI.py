9ff306e8aa332e6b97622cd7f1593b415cb361f8
Trace Tidwell <trace.tidwell@gmail.com>
2019-11-23 15:03:46 -0500

    local changes

--- assignments/ps_5/computeImprovedMHI.py
+++ assignments/ps_5/computeImprovedMHI.py
@@ -6,33 +6,7 @@ import matplotlib.pyplot as plt
 from skimage.io import imread
 
 
-def computeImprovedMHI(directoryName, threshold=12000):
-
-    # depth_files = glob.glob(os.path.join(directoryName, '*.pgm'))
-    # depth_files = np.sort(depth_files)
-
-    # depth_ims = []
-    # for depth_file in depth_files:
-    #     depth_im = imread(depth_file)
-    #     depth_ims.append(depth_im)
-
-    # foreground_ims = []
-    # for depth_im in depth_ims:
-    #     foreground_im = depth_im.copy()
-    #     foreground_im[foreground_im > threshold] = 0
-    #     foreground_ims.append(foreground_im)
-
-    # difference_ims = []
-    # for i in range(1, len(foreground_ims)):
-    #     foreground_im1 = foreground_ims[i-1].copy()
-    #     foreground_im2 = foreground_ims[i].copy()
-    #     foreground_im1[foreground_im1>0] = 1
-    #     foreground_im2[foreground_im2>0] = 1
-    #     difference_im = foreground_im2 - foreground_im1
-    #     difference_im[difference_im != 0] = 1
-    #     # difference_im[470:] = 0
-    #     # difference_im[250:, 550:] = 0
-    #     difference_ims.append(difference_im)
+def computeImprovedMHI(directoryName, threshold1=40000, threshold2=30000):
 
     depth_files = glob.glob(os.path.join(directoryName, '*.pgm'))
     depth_files = np.sort(depth_files)
@@ -42,20 +16,36 @@ def computeImprovedMHI(directoryName, threshold=12000):
         depth_im = imread(depth_file)
         depth_ims.append(depth_im)
 
+    # foreground_ims = []
+    # for i in range(1, len(depth_ims)):
+    #     diff = depth_ims[i] - depth_ims[i-1]
+    #     diff[diff >= threshold] = 0
+    #     diff[diff < threshold] = 1
+    #     foreground_ims.append(diff)
+
+    # foreground_ims = []
+    # for depth_im in depth_ims:
+    #     foreground_im = depth_im.copy()
+    #     foreground_im[foreground_im < threshold] = 1
+    #     foreground_im[foreground_im != 1] = 0
+    #     foreground_ims.append(foreground_im)
+
     foreground_ims = []
-    for depth_im in depth_ims:
-        foreground_im = depth_im.copy()
-        foreground_im[foreground_im < threshold] = 1
-        foreground_im[foreground_im != 1] = 0
-        foreground_ims.append(foreground_im)
+    for i in range(1, len(depth_ims)):
+        d0 = depth_ims[i-1].copy()
+        d1 = depth_ims[i].copy()
+        d0[d0 > threshold1] = 0
+        d1[d1 > threshold1] = 0
+        diff = np.abs(d1 - d0)
+        diff[diff < threshold2] = 0
+        diff[diff >= threshold2] = 1
+        foreground_ims.append(diff)
 
     mhi = np.zeros(foreground_ims[0].shape)
     for i, foreground_im in enumerate(foreground_ims):
         mhi[foreground_im == 1] = i + 1
         mhi[foreground_im == 0] -= 1
     mhi[mhi < 0] = 0
-    mhi[470:] = 0
-    mhi[250:, 525:] = 0
     mhi /= np.max(mhi)
 
     return mhi
